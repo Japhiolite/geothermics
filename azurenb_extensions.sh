7751684c77983b7f2d959713b2babd1c75b361a0
Japhiolite <jan.niederau@rwth-aachen.de>
2019-04-01 16:49:53 +0200

    Create azurenb_extensions.sh

---
+++ azurenb_extensions.sh
@@ -0,0 +1,5 @@
+jupyter nbextension enable codefolding/main
+jupyter nbextension enable collapsible_headings/main
+jupyter nbextension enable rubberband/main
+jupyter nbextension enable exercise/main
+jupyter nbextension enable exercise2/main
